a4edfd28ed6a3de42f6fcee34c27cae34dedd492
Johanny Amaya <jamayau@unal.edu.co>
2022-09-03 16:43:07 -0500

    ejercicio 1

---
+++ FizzBuzz hackerrank.py
@@ -0,0 +1,31 @@
+import hashlib
+
+
+
+def probar(n):
+    prueba=True
+    number=n
+    tres=""
+    cinco=""
+    if (n%3)==0:
+        tres="Fizz"
+        number=""
+        prueba=False
+    if (n%5)==0:
+        cinco="Buzz"
+        number=""
+        prueba=False
+    if prueba:
+        print(n)
+    else:
+        print(tres+cinco)
+
+def fizzBuzz(n):
+    # Write your code here
+    for i in range (n):
+        probar(i+1)
+        
+        
+numerito=int(input("ingrese numero"))
+
+fizzBuzz(numerito)
